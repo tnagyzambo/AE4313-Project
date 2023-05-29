b7fc91771952adc55eaeb78140882a25fd5f5323
Thomas Nagy Zambo <tnagyzambo@gmail.com>
2023-05-29 14:23:12 +0200

    Working LVLH tracking

--- plot.jl
+++ plot.jl
@@ -32,26 +32,28 @@ function plot_data(name, path)
     p1 = plot!(df.t, df.q3, label = L"q_3")
     p1 = plot!(df.t, df.q0, label = L"q_0")
 
-    #p2 = plot(df.t, df.w1, label = L"\omega_1", ylabel = L"\omega (rad/s)", legend = :topright, xaxis = false, bottom_margin = -5mm)
-    p2 = plot(df.t, df.w1, label = L"\omega_1", ylabel = L"\omega (rad/s)", xlabel = L"Time (s)", legend = :topright)
+    p2 = plot(df.t, df.w1, label = L"\omega_1", ylabel = L"\omega (rad/s)", legend = :topright, xaxis = false, bottom_margin = -5mm)
+    #p2 = plot(df.t, df.w1, label = L"\omega_1", ylabel = L"\omega (rad/s)", xlabel = L"Time (s)", legend = :topright)
     p2 = plot!(df.t, df.w2, label = L"\omega_2")
     p2 = plot!(df.t, df.w3, label = L"\omega_3")
    
-    #p3 = plot(df.t, df.u1, label = L"u_1", ylims = (0, 7), xlabel = L"Time (s)", ylabel = L"u (Nm)", legend = :topright)
+    p3 = plot(df.t, df.u1, label = L"u_1", xlabel = L"Time (s)", ylabel = L"u (Nm)", legend = :topright)
+    p3 = plot!(df.t, df.u2, label = L"u_2")
+    p3 = plot!(df.t, df.u3, label = L"u_3")
 
-    #plot(p1, p2, p3, size = (1000.0, 1000.0), layout = grid(3, 1, heights = (0.5, 0.25, 0.25)))
-    plot(p1, p2, size = (1000.0, 1000.0), layout = grid(2, 1, heights = (0.66, 0.33)))
+    plot(p1, p2, p3, size = (1000.0, 1000.0), layout = grid(3, 1, heights = (0.5, 0.25, 0.25)))
+    #plot(p1, p2, size = (1000.0, 1000.0), layout = grid(2, 1, heights = (0.66, 0.33)))
     savefig("images/" * name * "_attiude.png")
 
-    p1 = plot(df.t, df.q1_lvlh, label = L"q_1", ylabel = L"|q|", legend = :topright, xaxis = false, bottom_margin = -5mm, dpi=800)
-    p1 = plot!(df.t, df.q2_lvlh, label = L"q_2")
-    p1 = plot!(df.t, df.q3_lvlh, label = L"q_3")
-    p1 = plot!(df.t, df.q0_lvlh, label = L"q_0")
+    p1 = plot(df.t, df.q1_b_lvlh, label = L"q_1", ylabel = L"|q|", legend = :topright, xaxis = false, bottom_margin = -5mm, dpi=800)
+    p1 = plot!(df.t, df.q2_b_lvlh, label = L"q_2")
+    p1 = plot!(df.t, df.q3_b_lvlh, label = L"q_3")
+    p1 = plot!(df.t, df.q0_b_lvlh, label = L"q_0")
 
     #p2 = plot(df.t, df.w1, label = L"\omega_1", ylabel = L"\omega (rad/s)", legend = :topright, xaxis = false, bottom_margin = -5mm)
-    p2 = plot(df.t, df.w1_lvlh, label = L"\omega_1", ylabel = L"\omega (rad/s)", xlabel = L"Time (s)", legend = :topright)
-    p2 = plot!(df.t, df.w2_lvlh, label = L"\omega_2")
-    p2 = plot!(df.t, df.w3_lvlh, label = L"\omega_3")
+    p2 = plot(df.t, df.w1_b_lvlh, label = L"\omega_1", ylabel = L"\omega (rad/s)", xlabel = L"Time (s)", legend = :topright)
+    p2 = plot!(df.t, df.w2_b_lvlh, label = L"\omega_2")
+    p2 = plot!(df.t, df.w3_b_lvlh, label = L"\omega_3")
    
     #p3 = plot(df.t, df.u1, label = L"u_1", ylims = (0, 7), xlabel = L"Time (s)", ylabel = L"u (Nm)", legend = :topright)
 
@@ -60,7 +62,7 @@ function plot_data(name, path)
     savefig("images/" * name * "_attiude_lvlh.png")
 
 
-    t_o = 5926
+    t_o = 5100
     r_o = 1000e3
     n_o = 1
 
@@ -71,9 +73,12 @@ function plot_data(name, path)
         plot(df_orbit.x / r_o, df_orbit.y / r_o, df_orbit.z / r_o, formatter=(_...) -> "", legend = :none, dpi=800)
 
         df_orbit = filter!(row-> (row.t % 150 == 0), df_orbit)
-        transform!(df_orbit, [:q0_lvlh, :q1_lvlh, :q2_lvlh, :q3_lvlh] => ByRow((q0, q1, q2, q3) -> (QuatRotation(q0, q1, q2, q3) * SVector(1.0, 0.0, 0.0))) => [:x_body_x, :x_body_y, :x_body_z])
-        transform!(df_orbit, [:q0_lvlh, :q1_lvlh, :q2_lvlh, :q3_lvlh] => ByRow((q0, q1, q2, q3) -> (QuatRotation(q0, q1, q2, q3) * SVector(0.0, 1.0, 0.0))) => [:y_body_x, :y_body_y, :y_body_z])
-        transform!(df_orbit, [:q0_lvlh, :q1_lvlh, :q2_lvlh, :q3_lvlh] => ByRow((q0, q1, q2, q3) -> (QuatRotation(q0, q1, q2, q3) * SVector(0.0, 0.0, 1.0))) => [:z_body_x, :z_body_y, :z_body_z])
+        transform!(df_orbit, [:q0, :q1, :q2, :q3] => ByRow((q0, q1, q2, q3) -> (QuatRotation(q0, q1, q2, q3) * SVector(1.0, 0.0, 0.0))) => [:x_body_x, :x_body_y, :x_body_z])
+        transform!(df_orbit, [:q0, :q1, :q2, :q3] => ByRow((q0, q1, q2, q3) -> (QuatRotation(q0, q1, q2, q3) * SVector(0.0, 1.0, 0.0))) => [:y_body_x, :y_body_y, :y_body_z])
+        transform!(df_orbit, [:q0, :q1, :q2, :q3] => ByRow((q0, q1, q2, q3) -> (QuatRotation(q0, q1, q2, q3) * SVector(0.0, 0.0, 1.0))) => [:z_body_x, :z_body_y, :z_body_z])
+        # transform!(df_orbit, [:q0_lvlh, :q1_lvlh, :q2_lvlh, :q3_lvlh] => ByRow((q0, q1, q2, q3) -> (QuatRotation(q0, q1, q2, q3) * SVector(1.0, 0.0, 0.0))) => [:x_body_x, :x_body_y, :x_body_z])
+        # transform!(df_orbit, [:q0_lvlh, :q1_lvlh, :q2_lvlh, :q3_lvlh] => ByRow((q0, q1, q2, q3) -> (QuatRotation(q0, q1, q2, q3) * SVector(0.0, 1.0, 0.0))) => [:y_body_x, :y_body_y, :y_body_z])
+        # transform!(df_orbit, [:q0_lvlh, :q1_lvlh, :q2_lvlh, :q3_lvlh] => ByRow((q0, q1, q2, q3) -> (QuatRotation(q0, q1, q2, q3) * SVector(0.0, 0.0, 1.0))) => [:z_body_x, :z_body_y, :z_body_z])
         
         scatter!(df_orbit.x / r_o, df_orbit.y / r_o, df_orbit.z / r_o, markersize = 0.2)
         arrow3d!(df_orbit.x / r_o, df_orbit.y / r_o, df_orbit.z / r_o, df_orbit.x_body_x, df_orbit.x_body_y, df_orbit.x_body_z; as=-0.01, lc=:red, la=0.5)
--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ use tracing_subscriber;
 fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
-    let t_orbit = 5926.0;
-    let n_orbit = 3.0;
+    let t_orbit = 5100.0;
+    let n_orbit = 1.0;
     let t_start = 0.0;
     let t_end = t_orbit * n_orbit;
     let dt_0 = 0.1;
@@ -18,10 +18,10 @@ fn main() -> Result<()> {
         .transform_vector(&SVector::<f64, 3>::new(7078.0e3, 0.0, 0.0)); // Initial orbital position
     let v_0 = UnitQuaternion::from_euler_angles(0.0, -15.0, 0.0)
         .transform_vector(&SVector::<f64, 3>::new(0.0, 7.5043e3, 0.0)); // Initial orbital velocity
-    let q_0 = UnitQuaternion::from_euler_angles(10.0, 10.0, 10.0); // Initial satellite attitude
+    let q_0 = Quaternion::new(1.0, 0.0, 0.0, 0.0); // Initial satellite attitude
     let w_0 = SVector::<f64, 3>::new(0.0, 0.0, 0.0); // Initial body angular velocity
 
-    let x_0 = SVector::from([
+    let mut x_0 = State::from(SVector::from([
         r_0.index(0).to_owned(),
         r_0.index(1).to_owned(),
         r_0.index(2).to_owned(),
@@ -35,7 +35,13 @@ fn main() -> Result<()> {
         w_0.index(0).to_owned(),
         w_0.index(1).to_owned(),
         w_0.index(2).to_owned(),
-    ]);
+    ]));
+    x_0.set_q_b_in_lvlh(
+        UnitQuaternion::from_euler_angles(10.0, 10.0, 10.0)
+            .quaternion()
+            .to_owned(),
+    );
+
     let mut t = t_start;
     let mut t_prev_control_step = t_start;
 
@@ -48,31 +54,61 @@ fn main() -> Result<()> {
     let mut observer = Observer {};
 
     let mut controller = Controller {
-        u: SVector::from([0.0, 0.0, 0.0]),
+        k_p: 1.0,
+        k_d: 10.0,
+        //k_p: 0.0,
+        //k_d: 0.0,
+        u: SVector::<f64, 3>::new(0.0, 0.0, 0.0),
     };
 
     let mut wtr = csv::Writer::from_path("out.csv")?;
     wtr.write_record(&[
-        "t", "x", "y", "z", "v1", "v2", "v3", "q1", "q2", "q3", "q0", "w1", "w2", "w3", "q1_lvlh",
-        "q2_lvlh", "q3_lvlh", "q0_lvlh", "w1_lvlh", "w2_lvlh", "w3_lvlh", "obv", "u1", "u2", "u3",
+        "t",
+        "x",
+        "y",
+        "z",
+        "v1",
+        "v2",
+        "v3",
+        "q1",
+        "q2",
+        "q3",
+        "q0",
+        "w1",
+        "w2",
+        "w3",
+        "q1_lvlh",
+        "q2_lvlh",
+        "q3_lvlh",
+        "q0_lvlh",
+        "q1_b_lvlh",
+        "q2_b_lvlh",
+        "q3_b_lvlh",
+        "q0_b_lvlh",
+        "w1_b_lvlh",
+        "w2_b_lvlh",
+        "w3_b_lvlh",
+        "u1",
+        "u2",
+        "u3",
     ])?;
-    let mut x_obv = observer.step(SVector::<f64, 1>::new(0.0));
-    let mut u = controller.step(x_obv);
     let mut x = x_0;
+    let mut x_obv = observer.step(&x);
     let mut dt = dt_0;
-    wtr.serialize((t, x, lvlh(x), x_obv, u))?;
+    controller.step(&x);
+    wtr.serialize((t, x.x, x.q_lvlh(), x.q_b_lvlh(), x.w_b_lvlh(), controller.u))?;
 
     // Loop
     while t < t_end {
-        let (x_new, dt_new) = solver.step(x, u, combined, t, dt)?;
+        let (x_new, dt_new) = solver.step(&x, controller.u, combined, t, dt)?;
 
         let t_next_control_step = t_prev_control_step + dt_control;
 
         if (t + dt) > t_next_control_step {
-            (x, dt) = solver.step_to(x, u, combined, t, t_next_control_step);
+            (x, dt) = solver.step_to(x, controller.u, combined, t, t_next_control_step);
             t = t + dt;
-            x_obv = observer.step(SVector::<f64, 1>::new(0.0));
-            u = controller.step(x_obv);
+            x_obv = observer.step(&x);
+            controller.step(&x);
             t_prev_control_step = t;
         } else {
             t = t + dt;
@@ -83,40 +119,18 @@ fn main() -> Result<()> {
         }
         dt = dt_new;
 
-        wtr.serialize((t, x, lvlh(x), x_obv, u))?;
+        wtr.serialize((t, x.x, x.q_lvlh(), x.q_b_lvlh(), x.w_b_lvlh(), controller.u))?;
     }
 
     wtr.flush()?;
     Ok(())
 }
 
-fn combined(x: SVector<f64, 13>, u: SVector<f64, 3>) -> SVector<f64, 13> {
-    let r = SVector::<f64, 3>::from([
-        x.index(0).to_owned(),
-        x.index(1).to_owned(),
-        x.index(2).to_owned(),
-    ]);
-    let v = SVector::<f64, 3>::from([
-        x.index(3).to_owned(),
-        x.index(4).to_owned(),
-        x.index(5).to_owned(),
-    ]);
-    let q = Quaternion::from([
-        x.index(6).to_owned(),
-        x.index(7).to_owned(),
-        x.index(8).to_owned(),
-        x.index(9).to_owned(),
-    ]);
-    let w = SVector::<f64, 3>::from([
-        x.index(10).to_owned(),
-        x.index(11).to_owned(),
-        x.index(12).to_owned(),
-    ]);
-
-    let r_dot = orbit_kinematics(r, v);
-    let v_dot = orbit_dynamics(r, v);
-    let q_dot = attitude_kinematics(q, w);
-    let w_dot = attitude_dynamics(q, w, r, u);
+fn combined(x: &State, u: SVector<f64, 3>) -> SVector<f64, 13> {
+    let r_dot = orbit_kinematics(x.r(), x.v());
+    let v_dot = orbit_dynamics(x.r(), x.v());
+    let q_dot = attitude_kinematics(x.q(), x.w());
+    let w_dot = attitude_dynamics(x.q(), x.w(), x.r(), u);
 
     let x_dot = SVector::<f64, 13>::from([
         r_dot.index(0).to_owned(),
@@ -191,7 +205,9 @@ fn attitude_dynamics(
     //return inertia.try_inverse().unwrap()
     //    * ((-w).cross(&(inertia * w)) + torque_gg(r, inertia) + torque_d());
 
-    return inertia.try_inverse().unwrap() * ((-w).cross(&(inertia * w)));
+    return inertia.try_inverse().unwrap() * (-(w).cross(&(inertia * w)) + u);
+
+    //return inertia.try_inverse().unwrap() * ((-w).cross(&(inertia * w)));
 }
 
 /// Gravity gradient torque
@@ -212,101 +228,131 @@ fn torque_d() -> SVector<f64, 3> {
     return SVector::<f64, 3>::new(0.001, 0.001, 0.001);
 }
 
-fn lvlh(x: SVector<f64, 13>) -> (Quaternion<f64>, SVector<f64, 3>) {
-    let r = SVector::<f64, 3>::from([
-        x.index(0).to_owned(),
-        x.index(1).to_owned(),
-        x.index(2).to_owned(),
-    ]);
-
-    let v = SVector::<f64, 3>::from([
-        x.index(3).to_owned(),
-        x.index(4).to_owned(),
-        x.index(5).to_owned(),
-    ]);
-
-    let q = Quaternion::from([
-        x.index(6).to_owned(),
-        x.index(7).to_owned(),
-        x.index(8).to_owned(),
-        x.index(9).to_owned(),
-    ]);
-
-    let w = SVector::<f64, 3>::from([
-        x.index(10).to_owned(),
-        x.index(11).to_owned(),
-        x.index(12).to_owned(),
-    ]);
-
-    let w_o = SVector::<f64, 3>::new(0.0, v.norm() / r.norm(), 0.0);
-
-    let r = -r.normalize();
-    let v = v.normalize();
-
-    //let rot = Rotation3::from_basis_unchecked(&[v, n, r]);
-
-    //let angle = rot.angle() / 2.0;
-    //let axis = match rot.axis() {
-    //    Some(axis) => axis,
-    //    None => Vector3::x_axis(),
-    //};
-
-    //let q_lvlh = Quaternion::new(
-    //    angle.cos(),
-    //    axis[0] * angle.sin(),
-    //    axis[1] * angle.sin(),
-    //    axis[2] * angle.sin(),
-    //);
-
-    //let w_lvlh = q_lvlh * Quaternion::new(0.0, w[0], w[1], w[2]) * q_lvlh.conjugate();
-    //let w_lvlh = SVector::<f64, 3>::new(w_lvlh.i, w_lvlh.j, w_lvlh.k);
-
-    let z_eci = SVector::<f64, 3>::new(0.0, 0.0, 1.0);
-    let a1 = z_eci.cross(&r);
-    let q1 = Quaternion::new(1.0 + z_eci.dot(&r), a1[0], a1[1], a1[2]).normalize();
-
-    let x_eci = Quaternion::new(0.0, 1.0, 0.0, 0.0);
-    let x_q1 = q1 * x_eci * q1.conjugate();
-    let x_q1 = SVector::<f64, 3>::new(x_q1.i, x_q1.j, x_q1.k);
-
-    // Dot product provides no information if vector A is clockwise or counter-clockwise from vector B
-    let angle: f64;
-    if (x_q1.cross(&v) * r.transpose())[0].asin() > 0.0 {
-        angle = (x_q1.dot(&v)).acos() / 2.0;
-    } else {
-        angle = -(x_q1.dot(&v)).acos() / 2.0;
-    }
-    let axis = r;
-    let q2 = Quaternion::new(
-        angle.cos(),
-        axis[0] * angle.sin(),
-        axis[1] * angle.sin(),
-        axis[2] * angle.sin(),
-    );
-
-    let q_lvlh = q2 * q1;
-    let w_lvlh = q_lvlh * Quaternion::new(0.0, w[0], w[1], w[2]) * q_lvlh.conjugate();
-    let w_lvlh = SVector::<f64, 3>::new(w_lvlh.i, w_lvlh.j, w_lvlh.k) + w_o;
-
-    return (q_lvlh, w_lvlh);
+#[derive(Clone, Copy)]
+struct State {
+    x: SVector<f64, 13>,
 }
 
-struct Observer<const N: usize> {}
-
-impl<const N: usize> Observer<N> {
-    fn step(&self, x: SVector<f64, N>) -> SVector<f64, N> {
-        return x;
+impl From<SVector<f64, 13>> for State {
+    fn from(x: SVector<f64, 13>) -> Self {
+        Self { x: x }
     }
 }
 
-struct Controller<const N: usize> {
+impl State {
+    fn r(&self) -> SVector<f64, 3> {
+        return SVector::<f64, 3>::from([
+            self.x.index(0).to_owned(),
+            self.x.index(1).to_owned(),
+            self.x.index(2).to_owned(),
+        ]);
+    }
+
+    fn v(&self) -> SVector<f64, 3> {
+        return SVector::<f64, 3>::from([
+            self.x.index(3).to_owned(),
+            self.x.index(4).to_owned(),
+            self.x.index(5).to_owned(),
+        ]);
+    }
+
+    fn q(&self) -> Quaternion<f64> {
+        return Quaternion::from([
+            self.x.index(6).to_owned(),
+            self.x.index(7).to_owned(),
+            self.x.index(8).to_owned(),
+            self.x.index(9).to_owned(),
+        ]);
+    }
+
+    fn w(&self) -> SVector<f64, 3> {
+        return SVector::<f64, 3>::from([
+            self.x.index(10).to_owned(),
+            self.x.index(11).to_owned(),
+            self.x.index(12).to_owned(),
+        ]);
+    }
+
+    fn q_lvlh(&self) -> Quaternion<f64> {
+        let r = -self.r().normalize();
+        let v = self.v().normalize();
+
+        let z_eci = SVector::<f64, 3>::new(0.0, 0.0, 1.0);
+        let a1 = z_eci.cross(&r);
+        let q1 = Quaternion::new(1.0 + z_eci.dot(&r), a1[0], a1[1], a1[2]).normalize();
+
+        let x_eci = Quaternion::new(0.0, 1.0, 0.0, 0.0);
+        let x_q1 = q1 * x_eci * q1.conjugate();
+        let x_q1 = SVector::<f64, 3>::new(x_q1.i, x_q1.j, x_q1.k);
+
+        // Dot product provides no information if vector A is clockwise or counter-clockwise from vector B
+        let angle: f64;
+        if (x_q1.cross(&v) * r.transpose())[0].asin() > 0.0 {
+            angle = (x_q1.dot(&v)).acos() / 2.0;
+        } else {
+            angle = -(x_q1.dot(&v)).acos() / 2.0;
+        }
+        let axis = r;
+        let q2 = Quaternion::new(
+            angle.cos(),
+            axis[0] * angle.sin(),
+            axis[1] * angle.sin(),
+            axis[2] * angle.sin(),
+        );
+
+        return (q2 * q1).normalize();
+    }
+
+    fn q_b_lvlh(&self) -> Quaternion<f64> {
+        return self.q() * self.q_lvlh().conjugate();
+    }
+
+    fn w_b_lvlh(&self) -> SVector<f64, 3> {
+        let w_o = SVector::<f64, 3>::new(0.0, self.v().norm() / self.r().norm(), 0.0);
+        //let w_o = self.v().norm() / self.r().norm() * self.v().cross(&self.r());
+        let w_o = self.q_b_lvlh()
+            * Quaternion::new(0.0, w_o[0], w_o[1], w_o[2])
+            * self.q_b_lvlh().conjugate();
+        let w_o = SVector::<f64, 3>::new(w_o.i, w_o.j, w_o.k);
+
+        return self.w() - w_o;
+    }
+
+    fn set_q_b_in_lvlh(&mut self, q_b: Quaternion<f64>) {
+        let q_b_in_lvlh = self.q_lvlh();
+
+        self.x[6] = q_b_in_lvlh.i;
+        self.x[7] = q_b_in_lvlh.j;
+        self.x[8] = q_b_in_lvlh.k;
+        self.x[9] = q_b_in_lvlh.w;
+    }
+}
+
+struct Observer {}
+
+impl Observer {
+    fn step(&self, x: &State) -> State {
+        return x.to_owned();
+    }
+}
+
+struct Controller {
+    k_p: f64,
+    k_d: f64,
     u: SVector<f64, 3>,
 }
 
-impl<const N: usize> Controller<N> {
-    fn step(&mut self, x: SVector<f64, N>) -> SVector<f64, 3> {
-        self.u = self.u + SVector::from([1.0, 0.0, 0.0]);
-        self.u
+impl Controller {
+    fn step(&mut self, x: &State) {
+        // Body axes to ECI
+        //let q_e = x.q.conjugate();
+        //let w_e = x.w;
+
+        // Body axes to LVLH
+        let q_e = x.q_b_lvlh();
+        let w_e = x.w();
+
+        self.u = -self.k_d * w_e - self.k_p * SVector::<f64, 3>::new(q_e.i, q_e.j, q_e.k);
     }
 }
 
@@ -362,37 +408,45 @@ impl std::fmt::Display for SolverError {
 
 impl std::error::Error for SolverError {}
 
-struct RK45Increments<const N: usize> {
-    k1: SVector<f64, N>,
-    k2: SVector<f64, N>,
-    k3: SVector<f64, N>,
-    k4: SVector<f64, N>,
-    k5: SVector<f64, N>,
-    k6: SVector<f64, N>,
+struct RK45Increments {
+    k1: SVector<f64, 13>,
+    k2: SVector<f64, 13>,
+    k3: SVector<f64, 13>,
+    k4: SVector<f64, 13>,
+    k5: SVector<f64, 13>,
+    k6: SVector<f64, 13>,
 }
 
 /// Reference found [here](https://en.wikipedia.org/wiki/Runge%E2%80%93Kutta%E2%80%93Fehlberg_methoxd).
-struct RK45<const N: usize, const M: usize> {
+struct RK45<const M: usize> {
     dt_min: f64,
     dt_max: f64,
     tol: f64,
 }
 
-impl<const N: usize, const M: usize> RK45<N, M> {
+impl<const M: usize> RK45<M> {
     fn compute_increments(
         &self,
-        x: SVector<f64, N>,
+        x: &State,
         u: SVector<f64, M>,
-        df: fn(SVector<f64, N>, SVector<f64, M>) -> SVector<f64, N>,
+        df: fn(&State, SVector<f64, M>) -> SVector<f64, 13>,
         t: f64,
         dt: f64,
-    ) -> RK45Increments<N> {
+    ) -> RK45Increments {
         let k1 = dt * (df)(x, u);
-        let k2 = dt * (df)(x + B21 * k1, u);
-        let k3 = dt * (df)(x + B31 * k1 + B32 * k2, u);
-        let k4 = dt * (df)(x + B41 * k1 + B42 * k2 + B43 * k3, u);
-        let k5 = dt * (df)(x + B51 * k1 + B52 * k2 + B53 * k3 + B54 * k4, u);
-        let k6 = dt * (df)(x + B61 * k1 + B62 * k2 + B63 * k3 + B64 * k4 + B65 * k5, u);
+        let k2 = dt * (df)(&State::from(x.x + B21 * k1), u);
+        let k3 = dt * (df)(&State::from(x.x + B31 * k1 + B32 * k2), u);
+        let k4 = dt * (df)(&State::from(x.x + B41 * k1 + B42 * k2 + B43 * k3), u);
+        let k5 = dt
+            * (df)(
+                &State::from(x.x + B51 * k1 + B52 * k2 + B53 * k3 + B54 * k4),
+                u,
+            );
+        let k6 = dt
+            * (df)(
+                &State::from(x.x + B61 * k1 + B62 * k2 + B63 * k3 + B64 * k4 + B65 * k5),
+                u,
+            );
 
         RK45Increments {
             k1,
@@ -404,20 +458,18 @@ impl<const N: usize, const M: usize> RK45<N, M> {
         }
     }
 
-    fn compute_solution(
-        &self,
-        x: SVector<f64, N>,
-        increments: &RK45Increments<N>,
-    ) -> SVector<f64, N> {
-        x + CH1 * increments.k1
-            + CH2 * increments.k2
-            + CH3 * increments.k3
-            + CH4 * increments.k4
-            + CH5 * increments.k5
-            + CH6 * increments.k6
+    fn compute_solution(&self, x: &State, increments: &RK45Increments) -> State {
+        State::from(
+            x.x + CH1 * increments.k1
+                + CH2 * increments.k2
+                + CH3 * increments.k3
+                + CH4 * increments.k4
+                + CH5 * increments.k5
+                + CH6 * increments.k6,
+        )
     }
 
-    fn compute_truncation_error(&self, increments: &RK45Increments<N>) -> f64 {
+    fn compute_truncation_error(&self, increments: &RK45Increments) -> f64 {
         (CT1 * increments.k1
             + CT2 * increments.k2
             + CT3 * increments.k3
@@ -453,14 +505,14 @@ impl<const N: usize, const M: usize> RK45<N, M> {
 
     fn step(
         &self,
-        x: SVector<f64, N>,
+        x: &State,
         u: SVector<f64, M>,
-        df: fn(SVector<f64, N>, SVector<f64, M>) -> SVector<f64, N>,
+        df: fn(&State, SVector<f64, M>) -> SVector<f64, 13>,
         t: f64,
         dt: f64,
-    ) -> Result<(SVector<f64, N>, f64), SolverError> {
-        let increments = self.compute_increments(x, u, df, t, dt);
-        let solution = self.compute_solution(x, &increments);
+    ) -> Result<(State, f64), SolverError> {
+        let increments = self.compute_increments(&x, u, df, t, dt);
+        let solution = self.compute_solution(&x, &increments);
         let truncation_error = self.compute_truncation_error(&increments);
         let dt_new = self.compute_dt(dt, truncation_error)?;
 
@@ -474,16 +526,16 @@ impl<const N: usize, const M: usize> RK45<N, M> {
 
     fn step_to(
         &self,
-        x: SVector<f64, N>,
+        x: State,
         u: SVector<f64, M>,
-        df: fn(SVector<f64, N>, SVector<f64, M>) -> SVector<f64, N>,
+        df: fn(&State, SVector<f64, M>) -> SVector<f64, 13>,
         t: f64,
         t_target: f64,
-    ) -> (SVector<f64, N>, f64) {
+    ) -> (State, f64) {
         let dt = t_target - t;
 
-        let increments = self.compute_increments(x, u, df, t, dt);
-        let solution = self.compute_solution(x, &increments);
+        let increments = self.compute_increments(&x, u, df, t, dt);
+        let solution = self.compute_solution(&x, &increments);
         let truncation_error = self.compute_truncation_error(&increments);
 
         (solution, dt)
